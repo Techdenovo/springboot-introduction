7c05d4cd31a19b66071e73b0a7a3d5cf49b77334
Kalyan Mazumder <kalyaninform@gmail.com>
2020-09-17 12:24:35 +0530

    initial commit

---
+++ src/test/java/com/techdenovo/tut/springbootintroduction/SpringbootIntroductionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techdenovo.tut.springbootintroduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootIntroductionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
