49abc526c1d9cfe2246eb46145453c30556830b0
Kalyan Mazumder <kalyaninform@gmail.com>
2020-09-17 15:36:14 +0530

    added jsp with time

--- src/main/java/com/techdenovo/tut/springbootintroduction/controller/HomeController.java
+++ src/main/java/com/techdenovo/tut/springbootintroduction/controller/HomeController.java
@@ -1,14 +1,17 @@
 package com.techdenovo.tut.springbootintroduction.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-//@Controller
+@Controller
 //@RestController
-public class IndexController {
-//    @RequestMapping("/")
-    public String welcome(){
+public class HomeController {
+    @RequestMapping("/hi")
+    public String welcome(ModelMap model){
         return "index";
     }
 }
